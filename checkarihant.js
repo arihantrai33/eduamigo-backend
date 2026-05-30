e669df7956d8d8c4abfb158d4120419858b1516b
Arihant Rai <arihantrai33@gmail.com>
2026-05-31 03:49:43 +0530

    chore: remove temp debug scripts

--- checkarihant.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://eduamigo_admin:rai628444@cluster0.rmyiph4.mongodb.net/eduamigo');
-const Student = require('./src/models/Student');
-async function run() {
-  const student = await Student.findById('6a0bfa686fe7d3ba54c0d9cb');
-  console.log(JSON.stringify(student, null, 2));
-  process.exit();
-}
-run();
